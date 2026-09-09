8538b4922d6138f500cff30fa6078c73e41fea79
nahuelfranco7 <131535682+nahuelfranco7@users.noreply.github.com>
2026-09-08 21:35:25 -0300

    Creacion de clase Articulo e Imagen

---
+++ TPWinForm_equipo-i/Articulo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinForm_equipo_i
+{
+    internal class Articulo
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public Marca marca;
+
+        public Categoria categoria;
+
+        public List<Imagen> Imagenes = new List<Imagen>();
+    }
+}
---
+++ TPWinForm_equipo-i/Imagen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinForm_equipo_i
+{
+    internal class Imagen
+    {
+        public int Id { get; set; }
+        public int IdArticulo { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
